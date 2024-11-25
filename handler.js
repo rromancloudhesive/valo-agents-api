e71493838d0ac2c9c56d57ce269938bb3aa8660f
Ruben Roman <ruben.roman@cloudhesive.com>
2024-11-25 10:06:28 -0300

    :loud: console log deleted

--- handler.js
+++ handler.js
@@ -29,7 +29,7 @@ app.get('/agents', async (req, res) => {
   const params = {
     TableName: AGENTS_TABLE,
   };
-console.log('')
+  
   try {
     const command = new ScanCommand(params);
     const { Items } = await docClient.send(command);
